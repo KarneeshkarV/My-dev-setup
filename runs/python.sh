2b38be5e6de9de601e0cfcacfc32cfd406c3a3ed
Karneeshkar <karneeshkar68@gmail.com>
2025-04-30 22:10:24 +0530

    uv

--- runs/python.sh
+++ runs/python.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install python3.10 -y
 sudo apt install pip3
 curl -LsSf https://astral.sh/uv/install.sh | sh
-#  source $HOME/.local/bin/env (sh, bash, zsh)
+source $HOME/.local/bin/env 
     #source $HOME/.local/bin/env.fish (fish)
 
 
