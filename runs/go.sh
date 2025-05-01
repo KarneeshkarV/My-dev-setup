9a73fb9a5afea32ea572acdcc6990210a41b3023
Karneeshkar <karneeshkar68@gmail.com>
2025-05-01 12:28:56 +0530

    go change

--- runs/go.sh
+++ runs/go.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
-curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
 LINE='export PATH=$PATH:/usr/local/go/bin'
 
 # File to modify
